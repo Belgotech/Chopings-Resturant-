79cf280c93467a23394eab2a4dc64af701f48cf2
Belgotech <Belovedobiora2002@gmail.com>
2023-01-30 15:29:54 +0100

    adding crossorigin

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,13 @@ import axios, { Axios } from 'axios'
 async function getResult (querry) {
     const key = '4d08ce58e497460ba4bc0aabe5b17da4'
     try {
-    const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?key=${key} ${querry}`)
+    const res = await axios(`https://crossorigin.me/https://api.spoonacular.com/recipes/complexSearch?key=${key} ${querry}`)
     } catch (error){
         alert(error)
     }
 }
 
-// getResult('pizza')
+getResult('pizza')
 
 
 
