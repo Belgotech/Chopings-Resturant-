aa34bf875867c586fe3938653360cc9bd0ece6a9
Belgotech <Belovedobiora2002@gmail.com>
2023-01-30 16:19:02 +0100

    api not working, working getting another API

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,11 @@ import axios, { Axios } from 'axios'
 
 async function getResult (querry) {
     const key = '4d08ce58e497460ba4bc0aabe5b17da4'
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
     try {
-    const res = await axios(`https://crossorigin.me/https://api.spoonacular.com/recipes/complexSearch?key=${key} ${querry}`)
+    const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key} ${querry}`)
+    // const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2`)
+    console.log(res)
     } catch (error){
         alert(error)
     }
@@ -16,3 +19,4 @@ getResult('pizza')
 
 //GET https://api.spoonacular.com/recipes/complexSearch
 // 4d08ce58e497460ba4bc0aabe5b17da4
+// https://cors-anywhere.herokuapp.com/
