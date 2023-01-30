4d6b525920bacd5f674bf415543f4235e2466021
Belgotech <Belovedobiora2002@gmail.com>
2023-01-30 14:22:15 +0100

    new changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
+import axios, { Axios } from 'axios'
+
+async function getResult (querry) {
+    const key = '4d08ce58e497460ba4bc0aabe5b17da4'
+    try{
+    const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?key=${key} ${querry}`)
+}
+}
+
+getResult('pizza')
+
 
 
 //GET https://api.spoonacular.com/recipes/complexSearch
