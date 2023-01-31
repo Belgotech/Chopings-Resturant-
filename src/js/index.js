31b0ef44a47b952d4b697c385f23c3559531a4c8
Belgotech <Belovedobiora2002@gmail.com>
2023-01-31 15:11:09 +0100

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,8 @@ async function getResult (querry) {
     const key = '4d08ce58e497460ba4bc0aabe5b17da4'
     const proxy = 'https://cors-anywhere.herokuapp.com/'
     try {
-    const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}?${querry}`)
-    // const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2`)
+    // const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}?${querry}`)
+    const res = await axios(`https://forkify-api.herokuapp.com/api/get?r?apiKey=${key}?${querry}`)
     console.log(res)
     } catch (error){
         alert(error)
